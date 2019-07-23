e54c5e923521550944fd6cb912abc940b17cb23a
mullarkb <mullarkb@tcd.ie>
2019-07-23 13:33:26 +0100

    details sidebar work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <Header></Header>
-    <TransactionInfo></TransactionInfo>
+    <div class="page-content">
+      <TransactionInfo></TransactionInfo>
+      <DetailsPanel></DetailsPanel>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import TransactionInfo from './components/TransactionInfo'
+import DetailsPanel from './components/DetailsPanel'
 
 export default {
   name: 'app',
   components: {
+    DetailsPanel,
     TransactionInfo,
     Header
   }
@@ -28,11 +33,22 @@ export default {
   margin: 0;
   padding: 0;
   font-size: 10px;
-
   p {
     font-size: 1.4rem;
     line-height: 1.5;
     margin: 0;
+  }
+  .page-content{
+    width:100%;
+    display: inline-flex;
+    #transaction-info{
+      width: 59%;
+    }
+    #details-panel{
+      width:41%;
+      background-color: #FBFBFB;
+      border-left: solid 1px #E0E0E0;
+    }
   }
 }
 </style>
--- src/components/DetailsPanel.vue
+++ src/components/DetailsPanel.vue
@@ -1,5 +1,38 @@
 <template>
-    
+  <div id='details-panel'>
+    <table class="details-table">
+      <tr class="table-head">
+        <td>Send Details</td>
+      </tr>
+      <tr class="you-send">
+        <td>You Send</td>
+        <td>€2,000.00</td>
+      </tr>
+      <tr class="table-head">
+        <td>Receiving Details</td>
+        <td class="info">As of right now</td>
+      </tr>
+      <tr class="table-content">
+        <td>Rate</td>
+        <td>0.86022</td>
+      </tr>
+      <tr class="table-content">
+        <td>Fee</td>
+        <td>£2.50</td>
+      </tr>
+      <tr class="table-content">
+        <td>Delivery Date</td>
+        <td>25th November</td>
+      </tr>
+      <tr class="table-content">
+        <td>Recipient Gets</td>
+        <td>£1717.94</td>
+      </tr>
+      <tr class="table-foot">
+        <td>You save $66.19 compared to your bank!</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -9,5 +42,49 @@
 </script>
 
 <style lang="less" scoped>
-
+  .details-table{
+    margin: 53px auto auto 56px;
+    max-width: 310px;
+    width: 100%;
+    border: solid 1px #E0E0E0;
+    border-radius: 3px;
+    border-collapse: collapse;
+    tr{
+      td{
+        padding: 20px;
+        font-size: 1.4rem;
+        line-height: 24px;
+        border-style: none;
+        padding: 20px;
+        &.info{
+          color: #64B5F6;
+          font-size: 1.2rem;
+        }
+      }
+      &.table-content, &.you-send{
+        background-color: #ffffff;
+        td{
+          color: #9E9E9E;
+        }
+      }
+      &.table-content td{
+        padding: 0 20px;
+      }
+      &.table-content:first-child td:first-child{
+        padding-top: 20px;
+      }
+      &.table-head{
+        color: #616161;
+        border: solid 1px #E0E0E0;
+      }
+      &.table-foot{
+        text-align: center;
+        border: solid 1px #E0E0E0;
+        td{
+          color: #FFC766;
+          font-size: 1.2rem;
+        }
+      }
+    }
+  }
 </style>
